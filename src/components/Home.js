1b2207b0120cccddbe1aa71770942e2e20be70e0
Cristian Buitrago <Crisis1882@gmail.com>
2021-08-20 13:50:04 -0500

    Movie grid, thumbnails and spinner finished

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 // API
 import API from '../API'
 // Config
@@ -6,6 +6,9 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config'
 
 // Components
 import HeroImage from './HeroImage'
+import Grid from './Grid'
+import Thumb from './Thumb'
+import Spinner from './Spinner'
 
 // Hook
 import {useHomeFetch} from '../hooks/useHomeFetch'
@@ -27,6 +30,21 @@ const Home = () => {
         text={state.results[0].overview}
       /> 
       ) : null}
+      <Grid header='Popular Movies'>
+        {state.results.map(movie => (
+          <Thumb 
+            key={movie.id} 
+            clickable
+            image={
+              movie.poster_path 
+                ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                : NoImage
+            }
+            movieId={movie.id}
+          />
+        ))}
+      </Grid>
+      <Spinner/>
     </>
   )
 }
