44f1e19a55e8cc2b403581b0bf1e502d3977a7b2
Cristian Buitrago <Crisis1882@gmail.com>
2021-08-20 16:22:17 -0500

    Basic routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
 
-//components
+// Routing
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Components
 import Header from './components/Header';
 import Home from './components/Home';
+import Movie from './components/Movie';
+import NotFound from './components/NotFound'
 
-//styles
+// Styles
 import { GlobalStyle } from './GlobalStyle';
 
-const App = () => {
-  return (
-    <div className="App">
+const App = () => (
+  <div className="App">
+    <Router>
       <Header />
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/:movieId' element={<Movie />} />
+        <Route path='/*' element={<NotFound />} />
+      </Routes>
       <GlobalStyle />
-    </div>
-  );
-}
+    </Router>
+  </div>
+);
 
 export default App;
