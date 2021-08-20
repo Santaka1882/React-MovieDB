38f0f801499fa367c127dd28ec9e746fc9dda4ae
Cristian Buitrago <Crisis1882@gmail.com>
2021-08-20 15:49:18 -0500

    SearchBar logic

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -37,8 +37,9 @@ export const useHomeFetch = () => {
 
   // Initial and search
   useEffect(() => {
-    fetchMovies(1)
-  }, [])
+    setState(initialState)
+    fetchMovies(1, searchTerm)
+  }, [searchTerm])
 
-  return { state, loading, error, setSearchTerm }
+  return { state, loading, error, searchTerm, setSearchTerm }
 }
